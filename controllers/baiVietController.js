a5cc9577b294618ea89cf6eb556ec23b04d074e3
dreamernguyen <55968031+dreamernguyen@users.noreply.github.com>
2021-01-24 19:38:25 +0700

    Update V1

---
+++ controllers/baiVietController.js
@@ -0,0 +1,28 @@
+<<<<<<< Updated upstream
+=======
+import BaiViet from "../models/baiViet.js";
+
+export async function dangBai(req, res) {
+  const baiViet = new BaiViet(req.body);
+  try {
+    await baiViet.save();
+    res.send({ thongBao: "Đăng bài thành công !" });
+    console.log("Đăng bài thành công !");
+  } catch (error) {
+    res.send({ thongBao: "Đăng bài không thành công !" });
+    console.log("Đăng bài không thành công !");
+  }
+}
+
+export async function danhSachBaiViet(req, res) {
+  await BaiViet.find()
+    .then((result) => {
+      res.send(result);
+      console.log("Danh sách bài viết");
+    })
+    .catch((err) => {
+      res.send({ thongBao: "Lỗi lấy danh sách bài viết" });
+      console.log("Lỗi " + err.message);
+    });
+}
+>>>>>>> Stashed changes
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import express from 'express'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
@@ -11,8 +12,73 @@ const databaseURL = 'mongodb+srv://nhannbt:nhanne@cluster0-hw1yh.mongodb.net/dbY
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended : false}))
+=======
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import http from "http";
+import {Server} from 'socket.io';
+import TinNhan from './models/tinNhan.js'
+
+const port = 3000;
+const app = express();
+const server = app.listen(port,()=>{
+  console.log('Ok')
+})
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log(`connect ${socket.id}`);
+
+  // socket.on("guiThongBao", (cb) => {
+  //   console.log("ping");
+  socket.on("testab",(hihi)=>{
+    console.log("kkkkk")
+  })
+  TinNhan.watch().on('change',(change)=>{
+    console.log('Something has changed')
+    // io.to(change.fullDocument._id).emit('changes',change.fullDocument)
+    io.emit("thongBao",change.fullDocument)
+})
+  
+   
+  // });
+
+  socket.on("disconnect", () => {
+    console.log(`disconnect ${socket.id}`);
+  });
+});
+
+
+
+
+import nguoiDungRouter from "./routers/nguoiDungRouter.js";
+import matHangRouter from "./routers/matHangRouter.js";
+import tinNhanRouter from "./routers/tinNhanRouter.js";
+import baiVietRouter from "./routers/baiVietRouter.js";
+import binhLuanRouter from "./routers/binhLuanRouter.js";
+
+
+
+// io.on('connection', () => { /* … */ });
+
+
+const databaseURL =
+  "mongodb+srv://nhannbt:nhanne@cluster0-hw1yh.mongodb.net/dbYoKaFo?retryWrites=true&w=majority";
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//cài đặt điều hướng
+app.use("/nguoiDung/", nguoiDungRouter);
+app.use("/matHang", matHangRouter);
+app.use("/tinNhan/", tinNhanRouter);
+app.use("/baiViet/",baiVietRouter );
+app.use("/binhLuan/",binhLuanRouter );
+>>>>>>> Stashed changes
 
 mongoose
+<<<<<<< Updated upstream
 .connect(databaseURL,{ useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>{
     console.log('Đã kết nối đến MongoDb')
@@ -28,3 +94,15 @@ app.use('/nguoiDung/',nguoiDungRouter)
 app.listen(port,(req,res)=>{
     console.log(`Đang chạy trên port ${port}`)
 })
+=======
+  .connect(databaseURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Đã kết nối đến MongoDb");
+  })
+  .catch((error) => {
+    console.log("Lỗi kết nối đến database\n" + error);
+  });
+
+export default server
+
+>>>>>>> Stashed changes
