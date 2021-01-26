313893c008ab3455b6a1fef1a9110c0ff19d4df3
tri <triqmpd03233@fpt.edu.vn>
2021-01-26 08:01:29 +0700

    aa

---
+++ routers/baiVietRouter.js
@@ -0,0 +1,23 @@
+<<<<<<< Updated upstream
+=======
+import express from 'express'
+import {
+  dangBai,
+  danhSachBaiViet,
+  xoaBaiViet,
+  anBaiViet,
+  danhSachBaiVietAn,
+  huyAnBaiViet,
+} from '../controllers/baiVietController.js';
+
+const router = express.Router();
+
+router.get('/danhSach',danhSachBaiViet)
+router.post('/dangBai',dangBai)
+router.post('/xoa/:id', xoaBaiViet)
+router.post('/an/:id', anBaiViet)
+router.post('/danhSachAn', danhSachBaiVietAn)
+router.post('/huyAn/:id', huyAnBaiViet)
+
+export default router;
+>>>>>>> Stashed changes
